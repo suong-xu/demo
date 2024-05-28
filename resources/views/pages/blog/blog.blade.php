b7f817fb8c57c4d20163ba8d731cc91f78e27af8
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-05-28 23:38:15 +0700

    Update blog.blade.php

--- resources/views/pages/blog/blog.blade.php
+++ resources/views/pages/blog/blog.blade.php
@@ -12,7 +12,7 @@
         <div class="col-xl-12 col-md-12 col-lg-12 pt-3">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="page-title">Lịch sử thanh toán</h3>
+                    <h3 class="page-title">Lịch sử thanh toán123</h3>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
